aca6abe4a4d0c7f7d29d48a6c42bcd3fb90d51a4
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2026-01-05 15:51:26 +0900

    0009

---
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -0,0 +1,11 @@
+package com.back.domain.post.post.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    public long count() {
+        return 0;
+    }
+}
